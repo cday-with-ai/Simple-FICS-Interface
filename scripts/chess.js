838e441dccca2bd6b8ca416d261f05402071940a
cday <cday@mail.com>
2025-05-28 18:06:31 -0400

    Added analysis screenshot.

--- scripts/chess.js
+++ scripts/chess.js
@@ -70,7 +70,7 @@ let gameState = { // Reset header
     perspective: Perspective.FREESTYLE,
     lastPerspective: null,
     doublePawnPushFile: '',
-    minutes: 0,
+    minutes: 5,
     increment: 0,
     whiteTimeSecs: 0,
     whiteClockDisplay: "",
